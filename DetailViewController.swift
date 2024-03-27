e9d5d4f6025f73fcc9d925938e214459086cae39
nl020 <nl020@bucknell.edu>
2024-03-26 20:01:20 -0400

    Complete Project 6

---
+++ DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  ios101-project6-tumblr
+//
+//  Created by Naing Oo Lwin on 3/26/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var blogImageView: UIImageView!
+    @IBOutlet weak var blogLabel: UITextView!
+    
+    var post: Post?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let post = post {
+            // Populate UI elements with post data
+            if let photo = post.photos.first {
+                let url = photo.originalSize.url
+                // Load image asynchronously
+                DispatchQueue.global().async {
+                    if let data = try? Data(contentsOf: url) {
+                        DispatchQueue.main.async { [weak self] in
+                            self?.blogImageView.image = UIImage(data: data)
+                        }
+                    }
+                }
+            }
+            // Remove HTML tags from post.caption
+            let captionWithoutHTML = post.caption.stringByStrippingHTML()
+            blogLabel.text = captionWithoutHTML
+        }
+    }
+}
+
+extension String {
+    func stringByStrippingHTML() -> String {
+        return replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Tumblr-Project-6
+# Project 6 - *Tumblr Details*
+
+Submitted by: **Nolan Lwin**
+
+In this project, I added the ability for users to navigate to a detail view by tapping a blog post in the main tumblr feed.
+
+Time spent: **2** hours spent in total
+
+## Required Features
+
+The following **required** functionalities are completed:
+
+- [x] Setup navigation to Detail Screen.
+- [x] Create Detail View UI.
+- [x] Pass data to the Detail View Controller.
+- [x] Add some polish and finishing touches to the UI!
+
+## Video Walkthrough
+
+
+
+## License
+
+    Copyright 2024 Nolan Lwin
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
